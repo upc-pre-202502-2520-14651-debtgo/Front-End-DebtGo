a9d10f9e5d9b38f368897df91b5162786e91a26f
Developer <danielj.chavarriz@gmail.com>
2025-12-01 15:32:35 -0500

    feat(debtgo): update front-end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-end-debtgo.onrender.com/swagger-ui/index.html'
+  apiUrl: 'https://back-end-debtgo.onrender.com/api/v1'
 };
